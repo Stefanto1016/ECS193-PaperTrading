0901f3aea4d3004488ec3dbf7d32190c5cee3626
Steven To <stefanto@Stevens-MacBook-Pro-2.local>
2023-02-07 21:35:55 -0800

    basic buttons to switch between 2 graphs

--- paper-trading/src/App.js
+++ paper-trading/src/App.js
@@ -3,22 +3,51 @@ import { useState } from 'react';
 import LineChart from './components/LineChart';
 
 function App() {
-  const [chartData, setChartData] = useState({
+  //const [chartData, setChartData] = useState(chartData1);
+
+  // Data for Chart 1
+  const chartData1 = {
+    // x-axis labels
     labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     datasets: [
       {
         label: "Cost of Stock in $ (x100)",
+        // corresponding y values
         data: [3.2, 2.7, 1.9, 2.5, 3.0, 3.1, 4.0, 3.5, 3.2, 3.6, 3.9, 3.4],
         fill: true,
         borderColor: "blue",
         tension: 0.1
       }
     ]
-  });
+  }
+
+  // Datafor Chart 2
+  const chartData2 = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: "Cost of Stock in $ (x100)",
+        data: [1.2, 1.7, 1.0, 1.8, 2.0, 1.8, 2.1],
+        fill: true,
+        borderColor: "red",
+        tension: 0.1
+      }
+    ]
+  };
+
+  const [chartData, setChartData] = useState(chartData1);
+
+  function DisplayChart(chartData) {
+    return <LineChart chartData={chartData}/>
+  }
 
   return (
     <div className="App">
-      <LineChart chartData={chartData} /> 
+      <button onClick={() => setChartData(chartData1)}> Chart 1 </button>
+      <button onClick={() => setChartData(chartData2)}> Chart 2 </button>
+      <div style={{width: 1000}}>
+        <LineChart chartData={chartData}/>
+      </div>
     </div>
   );
 }
