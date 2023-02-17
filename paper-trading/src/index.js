bd85aa5c374ba75044c170d4a8313335b5de7cc5
grkoziol <92752287+grkoziol@users.noreply.github.com>
2023-02-16 18:00:00 -0800

    Add files via upload

--- paper-trading/src/index.js
+++ paper-trading/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="679166132884-ir5rkoeo1debgih79aeoju0tp88k212p.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
