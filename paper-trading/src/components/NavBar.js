4a84f7b33cbc01ea2fb234ac68c2e1d1f0ddd570
Steven To <stefanto@Stevens-MacBook-Pro-2.local>
2023-02-18 03:34:47 -0800

    added buttons to navbar

--- paper-trading/src/components/NavBar.js
+++ paper-trading/src/components/NavBar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Button from'@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { minHeight } from '@mui/system';
 
 export default function NavBar() {
     return(
         <div className="NavBar">
-            <AppBar position="static">
+            <AppBar position="fixed">
                 <Toolbar>
-                    <Typography variant="title" color="inherit">
+                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
                         CompuTrade
                     </Typography>
+                    <Button color='inherit'> Portfolio </Button>
+                    <Button color='inherit'> Stocks </Button>
+                    <Button color='inherit'> Learning </Button>
+                    <Button color='inherit'> LogOut </Button>
                 </Toolbar>
             </AppBar>
+            <Toolbar/>
         </div>
     )
 }
