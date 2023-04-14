852ce7e16a161341b8c1161cb6831fbfbac3119a
grkoziol <92752287+grkoziol@users.noreply.github.com>
2023-04-14 15:57:13 -0700

    Users added and route protection

--- paper-trading/src/App.js
+++ paper-trading/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './components/NavBar';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import { useNavigate } from 'react-router';
 
 
-function App() {  
+function App() { 
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(localStorage.getItem("profile") == ""){
+        navigate("/login");
+    }else{
+        setProfile(localStorage.getItem("profile"))
+    }
+})
+
+const [profile, setProfile] = useState(null);
+
   return (
+    <div>
+      {profile ? (
     <div className="App">
       <NavBar/> 
       <Home/>
+    </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
