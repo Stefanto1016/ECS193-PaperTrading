f1e3133c3b525936c394435719fe3c8aa21faf70
KusukabeAsako <86934881+KusukabeAsako@users.noreply.github.com>
2023-04-28 16:58:33 -0700

    Fixed bug when adding accounts

--- ExpressServer/index.js
+++ ExpressServer/index.js
@@ -691,6 +691,7 @@ async function createAccount(userKey)
 {
     await database.addUser(userKey, 10000, [], 10000);
     userQueues.set(userKey, queue.createQueue());
+    userQueues.get(userKey).run();
     userChallengeQueues.set(userKey, queue.createQueue());
     userChallengeQueues.get(userKey).run();
     challenge.createDailyChallengeProfile(userKey);
