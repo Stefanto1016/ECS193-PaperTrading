b001dcce864f4f68b39267bb545457e8cb52c167
Garrett Hilyer <garretthilyer@gmail.com>
2023-06-03 17:28:04 -0700

    Adding Mongo Tests

--- ExpressServer/mongo/tests/cache.test.js
+++ ExpressServer/mongo/tests/cache.test.js
@@ -2,6 +2,7 @@ const db = require('../database.js');
 const cache = require('../cache.js');
 
 beforeAll(async () => await db.connect());
+beforeAll(async () => await cache.deleteCacheStock("AAPL"));
 
 afterAll(async () => await db.disconnect());
 
@@ -15,6 +16,27 @@ describe('cacheInfo Tests', () => {
         expect(stock[1]).toBe(134);
         expect(stock[2]).toBe(155);
         expect(stock[3]).toBe(100);
+    })
+
+    it('updateCacheStock Test - 1', async () => {
+        await cache.updateCacheStock("AAPL", [190]);
+        const stock = await cache.getHistory("AAPL");
+
+        expect(stock[0]).toBe(190);
+    })
+
+    it('updateCacheStock Test - 2', async () => {
+        await cache.updateCacheStock("XOM", [190]);
+        const stock = await cache.getHistory("XOM");
+
+        expect(stock).toBe(undefined);
+    })
+
+    it('deleteCacheStock Test', async () => {
+        await cache.deleteCacheStock("AAPL");
+        const stock = await cache.getHistory("AAPL");
+
+        expect(stock).toBe(undefined);
     })
 
 })
